7fee07e6cfca1e58d8e49c8e0be1393d01e1176e
Justin W <justinwei.dev@gmail.com>
2021-04-07 15:43:39 +1200

    added reset button functionality

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const etchPad = document.querySelector("#etch-div");
 
-const size = 16;
+let size = 16;
 
-etchPad.setAttribute('style', `grid-template-columns: repeat(${size},1fr); \
+function createPad() {
+    etchPad.setAttribute('style', `grid-template-columns: repeat(${size},1fr); \
                     grid-template-rows: repeat(${size},1fr)`);
-
-console.log("ssss");
-
-for (i = 1; i <= size; i++ ) {
-    console.log("hehah");
-    for (j = 1; j <= size; j++ ) {
-        console.log("hehah")
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('style', `grid-area: ${i}/${j}/${i+1}/${j+1}`);
-        newDiv.classList.add('pixel');
-        etchPad.appendChild(newDiv);
+    for (i = 1; i <= size; i++ ) {
+        console.log("hehah");
+        for (j = 1; j <= size; j++ ) {
+            console.log("hehah")
+            let newDiv = document.createElement('div');
+            newDiv.setAttribute('style', `grid-area: ${i}/${j}/${i+1}/${j+1}`);
+            newDiv.classList.add('pixel');
+            etchPad.appendChild(newDiv);
+        }
     }
 }
+
+function resetPad() {
+    while (etchPad.firstChild) {
+        etchPad.removeChild(etchPad.lastChild);
+    }
+    let valueAccepted = false;
+    while (!valueAccepted) {
+        size = prompt("Speficy size of new pad (must be 100 or lower):");
+        if (size <= 100) { valueAccepted = true; }
+    }
+    createPad();
+}
+
+createPad();
